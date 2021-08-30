672d0d8332fd4043754e5ddbc98380db7d84ac47
Borg93 <48671678+Borg93@users.noreply.github.com>
2021-08-30 20:47:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from typing import Dict
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI, Request, Depends
 from pydantic import BaseModel
-import uvicorn
-import os
-
 from classifer.model import Model, get_model
 
 app = FastAPI()
@@ -18,16 +15,19 @@ class SentimentResponse(BaseModel):
     score: float
 
 
+@app.get("/")
+async def root():
+    return {"message": "hello world"}
 
 @app.post("/predict", response_model=SentimentResponse)  #, 
-def predict(request: SentimentRequest, model: Model = Depends(get_model)):
+async def predict(request: SentimentRequest, model: Model = Depends(get_model)):
     res = model.predict(request.text)
     sentiment, score = res[0]['sequence'], res[0]['score']
     return SentimentResponse( sentiment = sentiment, score = score)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
 
 
     
