4892470ebb00e25d60eaa6b8f3327557177b1195
Borg93 <48671678+Borg93@users.noreply.github.com>
2021-08-30 17:47:32 +0200

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -22,4 +22,7 @@ def predict(request: SentimentRequest, model: Model = Depends(get_model)):
     res = model.predict(request.text)
     sentiment, score = res[0]['sequence'], res[0]['score']
     return SentimentResponse( sentiment = sentiment, score = score)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
     
